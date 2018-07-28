835bce833999a13685e789a2b81eeb1a39d2c7d3
IA Masum <iamasum133@gmail.com>
2018-07-28 11:36:19 +0600

    Learned Multiple Components

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>This is the new Component</h2>
+            </div>
+        );
+    }
+}
