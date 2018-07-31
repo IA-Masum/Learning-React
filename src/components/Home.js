7f3ff6b6e3fa115729f0f42be05135d9d6455ff2
IA Masum <iamasum133@gmail.com>
2018-07-31 20:02:56 +0600

    Learned Proptypes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,25 @@
 import React from "react";
+import PropsTypes from "prop-types";
 
 export class Home extends React.Component{
-    render(){
 
+    render(){
+        var p = this.props;
         return(
             <div className="container">
-                <p> Name : {this.props.name}, Age : {this.props.age}</p>
+                <p> Name : {p.name}, Age : {p.age}</p>
                 <p>User Object => Name : {this.props.User.name}</p>
                 <div>
                     <p>Hobbies : </p>
-                    <ul>{this.props.User.hobbies.map((hobby) => <li>{hobby}</li>)}</ul>
+                    <ul>{p.User.hobbies.map((hobby , i) => <li key={i}>{hobby}</li>)}</ul>
                 </div>
             </div>
         );
     }
 }
+
+Home.PropsTypes = {
+    name : PropsTypes.string,
+    age : PropsTypes.number,
+    User : PropsTypes.object
+};
